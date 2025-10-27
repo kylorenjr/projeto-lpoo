3c85b668127689981ca32af83487c6eb4916e041
Lucas M Ventura <lmventura311205@gmail.com>
2025-10-27 15:09:23 -0300

    Aumento do Mapa e do spawn

--- src/Mapa.java
+++ src/Mapa.java
@@ -7,14 +7,14 @@ public class Mapa {
 
     public Mapa() {
         this.caminho = new ArrayList<>();
-        // Define um caminho fixo e simples. Cada {x, y} é um ponto no grid.
+
         caminho.add(new int[]{0, 5}); // Ponto inicial (Spawn)
         caminho.add(new int[]{1, 5});
         caminho.add(new int[]{2, 5});
         caminho.add(new int[]{3, 5});
         caminho.add(new int[]{4, 5});
         caminho.add(new int[]{5, 5});
-        caminho.add(new int[]{6, 5}); // Aumentei o caminho
+        caminho.add(new int[]{6, 5});
         caminho.add(new int[]{7, 5});
         caminho.add(new int[]{8, 5}); // Ponto final (Base)
     }
@@ -27,9 +27,6 @@ public class Mapa {
         return inimigo.getWaypointIndex() >= this.caminho.size() - 1;
     }
 
-    /**
-     * NOVO: Verifica se uma coordenada (x, y) do grid faz parte do caminho.
-     */
     public boolean isLocalNoCaminho(int x, int y) {
         for (int[] ponto : caminho) {
             if (ponto[0] == x && ponto[1] == y) {
